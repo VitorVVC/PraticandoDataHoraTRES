31481e4b8689561858f0485f414e2546d0520f0f
Vitor <vitorvvc2005@gmail.com>
2023-10-18 17:19:14 -0300

    Adicionando novos métodos e comentários a classe pessoa

--- src/entities/Pessoa.java
+++ src/entities/Pessoa.java
@@ -1,19 +1,25 @@
 package entities;
 
-public class Pessoa {
+import java.time.LocalDate;
+import java.time.YearMonth;
 
+public class Pessoa {
+    // Criando a classe pessoa
+    // Variaveis de nome , dia , mes e ano
     private String name;
     private int dia;
     private int mes;
     private int ano;
 
+    // Construtor
     public Pessoa(String name, int dia, int mes, int ano) {
         this.name = name;
-        this.dia = dia;
-        this.mes = mes;
-        this.ano = ano;
+        this.dia = validarDia(dia,mes);
+        this.mes = validarMes(mes);
+        this.ano = validarInt(ano);
     }
 
+    // Getters & Setters
     public String getName() {
         return name;
     }
@@ -44,5 +50,35 @@ public class Pessoa {
 
     public void setAno(int ano) {
         this.ano = ano;
+    }
+
+
+        // Métodos copiados da classe principal para servir de auxilio para o construtor e set
+        // Porém de void foram para int para poderem retornar algo para atribuir um valor
+
+    // Usado como método para validar o mes para limitar uma integracão com meses não existentes
+    public int validarMes(int mes) {
+        if (mes < 0 || mes > 12) {
+            throw new IllegalArgumentException("Não aceitamos numeros abaixo de zero ou acima de 12.");
+        }
+        return mes;
+    }
+    // Método que vai pedir um dia como entrada e mes, referente ao mes ele irá conferir se o dia pode exisitir em tal mes
+    public int validarDia(int dia, int mes) {
+        // Valida o mes
+        validarMes(mes);
+        // Atribui a um int o mes do ano do LocalDate HOJE, get no ano e mes para puxar o tamanho do mes
+        int maxDias = YearMonth.of(LocalDate.now().getYear(), mes).lengthOfMonth();
+        // Se o dia for menor que 1 ou maior que a quantidade de dias no mes ele retorna um erro
+        if (dia < 1 || dia > maxDias) {
+            throw new IllegalArgumentException("So aceitamos valores o primeiro e ultimo dia do mes. ");
+        }
+        return dia;
+    }
+    public int validarInt(int valor) {
+        if (valor <= 0) {
+            throw new IllegalArgumentException("Não aceitamos numeros negativos.");
+        }
+        return valor;
     }
 }
